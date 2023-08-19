0741a39db8e0a594857c4f09704b8b557952edd5
Dileesha Weliwaththa <64683688+dileeshaweliwaththa@users.noreply.github.com>
2023-08-20 03:12:25 +0530

    Initial commit

---
+++ actions/actions.py
@@ -0,0 +1,62 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
+import requests
+
+
+class ActionDefaultFallback(Action):
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        # Get user message from Rasa tracker
+        user_message = tracker.latest_message.get('text')
+        print(user_message)
+
+        # def get_chatgpt_response(self, message):
+        url = 'https://api.openai.com/v1/chat/completions'
+        headers = {
+            'Authorization': 'Bearer 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',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'model': "gpt-3.5-turbo",
+            'messages': [
+                {'role': 'system', 'content': 'You are an AI assistant for the user. You help to solve user query'},
+                {'role': 'user', 'content': 'You: ' + user_message}
+                ],
+            'max_tokens': 100
+        }
+        response = requests.post(url, headers=headers, json=data)
+        # response = requests.post(api_url, headers=headers, json=data)
+
+        # Print the API response
+        print(response.text)
+
+        if response.status_code == 200:
+            chatgpt_response = response.json()
+            message = chatgpt_response['choices'][0]['message']['content']
+            dispatcher.utter_message(message)
+        else:
+            # Handle error
+            return "Sorry, I couldn't generate a response at the moment. Please try again later."
+
+            # Revert user message which led to fallback.
+        return [UserUtteranceReverted()]
---
+++ voiceTTS.py
@@ -0,0 +1,46 @@
+import requests
+from elevenlabs import generate, play
+
+# Set your API key here
+API_KEY_ELBS = "ce1843626b13073800e1c7739cce6fe9"
+
+# Replace with your Rasa server URL
+RASA_SERVER_URL = "http://localhost:5005/webhooks/rest/webhook"
+
+
+# elevenlabs implementation
+def speak(audio_text, voice="Bella", model="eleven_monolingual_v1"):
+    audio = generate(text=audio_text, voice=voice, model=model, api_key=API_KEY_ELBS)
+    play(audio)
+
+
+def make_response_humanlike(response_text):
+    # Add pauses and intonations for natural speech
+    response_text = " ".join(response_text.split())  # Remove extra spaces
+    response_text = response_text + ","  # Add a comma for a natural pause
+    return response_text
+
+
+# Prepare message for POST request
+message = "Hello"
+
+# Send POST request
+
+response = requests.post(RASA_SERVER_URL, json={"sender": "duck", "message": message})
+
+# Play the response message received from the server
+if response.status_code == 200:
+    response_data = response.json()
+    response_message = response_data[0]['text']  # Assuming the server response contains a 'text' field
+    speak(response_message)
+
+    # Enhance the response for natural speech
+    enhanced_response = make_response_humanlike(response_message)
+
+    speak(enhanced_response)
+
+    print("Bot : ", response_message)
+    print("Bot2 : ", enhanced_response)
+
+else:
+    print("Failed to get a valid response from the server.")
